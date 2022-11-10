b6a74eee250c7790d28b370780497ef4eafa509c
quocdatx3 <quocdatx3@gmail.com>
2022-11-10 22:24:51 +0700

    update
    giao diện 9/15
    logic 4/15
    connection 0/15

---
+++ Admin/asset/script/notification.js
@@ -0,0 +1,7 @@
+$(document).ready(readyFn);
+
+function readyFn() {
+    // Code to run when the document is ready.
+    $('#myTable').pageMe({pagerSelector:'#myPager',showPrevNext:true,hidePageNumbers:false,perPage:5});
+}
+
